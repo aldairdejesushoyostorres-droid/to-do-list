ce53e44a92cfc645a0416d87643690f26a0e830c
aldairhoyos <aldairdejesushoyostorres@gmail.com>
2025-11-30 19:33:27 +0100

    Adding task by name, description, date and priority was implemented

--- todolist.py
+++ todolist.py
@@ -1,3 +1,5 @@
+import datetime 
+
 def main():
     print("Welcome to your to-do list!")
     while True:
@@ -13,3 +15,75 @@ def main():
                 break
         except ValueError:
             print("\nYou have to select a valid numeric option")
+
+def add_task(tasks):
+    name = input("\nName of the task: ")
+    description = input("\nDescription of the task: ")
+    while True:
+        try:
+            print("\nNow, pick a date for completing this task:")
+            print("\n1) Today")
+            print("\n2) Tomorrow")
+            print("\n3) The day after tomorrow")
+            print("\n4) Choose a specific date")
+            option = int(input("\nOption: "))
+            if option not in {1,2,3,4}:
+                print("\nYou have to choose a valid option between the ones provided")
+            else:
+                break
+        except ValueError:
+            print("\nyou have to pick a valid numeric option")
+    date = datetime.date.today()
+    match option:
+        case 1:
+            pass
+        case 2:
+            date = date + datetime.timedelta(days=1)
+        case 3:
+            date = date + datetime.timedelta(days=2)
+        case 4:
+            while True:
+                try:
+                    day = int(input("\nPick a day: "))
+                    month = int(input("\nPick a month: "))
+                    year = int(input("\nPick a year: "))
+                    date = datetime.date(year, month, day)
+                    if date < datetime.date.today():
+                        print("\nUnless you have a time machine, you have to select another date")
+                        print("\nTry again, please")
+                    else:
+                        break
+                except ValueError:
+                    print("\nYou have to choose a valid date, please")
+    while True:
+        try:
+            print("\nPick the priority of this task")
+            print("\n1) Low")
+            print("\n2) Normal")
+            print("\n3) High")
+            priority = int(input("\nOption: "))
+            if priority not in {1,2,3}:
+                print("\nYou have to select one of the provided options")
+            else:
+                if priority == 1:
+                    priority = "Low"
+                elif priority == 2:
+                    priority = "Normal"
+                else:
+                    priority = "High"
+                break
+        except ValueError:
+            print("\nYou have to provide a valid numeric option")
+    
+    task = {
+        "name": name,
+        "description": description,
+        "date": date,
+        "priority": priority
+    }
+
+    tasks.append(task)
+
+tasks = []
+add_task(tasks)
+print(tasks)
